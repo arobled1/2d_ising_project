cbadeedf9f9cc83af8be82d705f17833339e9111
Alan Robledo <ar6138@nyu.edu>
2019-09-26 17:55:02 -0400

    added comments

--- pow_iter.py
+++ pow_iter.py
@@ -1,3 +1,24 @@
+#=============================================================================80
+# Description:
+# Finds the dominant eigenvalue and associated eigenvector of the transfer
+# matrix, as described in paper by Lim and Weare, using the Power Method,
+# aka Power Iteration.
+# The user must specify an intial vector to represent the dominant eigenvector.
+# The initial vector is usually chosen to be an array of ones or some unit
+# vector (e.g. [1 0 0]).
+# Python 3.7.4 was used.
+#===============================================================================
+# Author: Alan Robledo
+# Date: 9/26/19
+#===============================================================================
+# Output:
+# Iteration #: 161
+# Dominant eigenvalue is: 2.6145152555104674
+#===============================================================================
+# Note:
+#   Decreasing the value of the tolerance will mean more iterations needed for
+#   convergence.
+#===============================================================================
 import numpy as np
 
 def power_iter(max_iterations, tolerance, matrix, vec):
@@ -25,6 +46,8 @@ def power_iter(max_iterations, tolerance, matrix, vec):
             i = max_iterations + 1
         if i == max_iterations:
             print("Exceeded max iterations. Try a new initial vector.")
+            print("Dominant eigenvalue is:", eig_val)
+            print("Dominant eigenvector is:", vec)
     return eig_val, vec
 
 n = 5    # number of spins
@@ -60,7 +83,7 @@ for i in range((2**n)//2):
 # Max number of iterations (set by user)
 max_iter = 200
 # Tolerance (set by user)
-tol = 10**-4
+tol = 10**-3
 # Setting initial guess vector
 init_vec = np.zeros(d)
 init_vec[0] = 1
