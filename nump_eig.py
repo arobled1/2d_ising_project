f0af1aea776956cafd3037c69a7a65d37b613221
Alan Robledo <ar6138@nyu.edu>
2019-09-26 17:32:00 -0400

    added comments

--- nump_eig.py
+++ nump_eig.py
@@ -1,12 +1,27 @@
+#=============================================================================80
+# Description:
+# Finds the dominant eigenvalue of the transfer matrix, as described in paper
+# by Lim and Weare, using the _geev LAPACK routine, which is appropriately
+# named np.eig().
+# Python 3.7.4 was used.
+# Paper: https://epubs.siam.org/doi/abs/10.1137/15M1040827
+#===============================================================================
+# Author: Alan Robledo
+# Date: 9/26/19
+#===============================================================================
+# Output:
+# For n = 5:
+# Dominant eigenvalue is: (2.6132783525854255+0j)
+#===============================================================================
 import numpy as np
 from numpy import linalg as la
 
-n = 50    # number of spins
+n = 5   # number of spins
 d = 2**n  # matrix dimension dxd
 b = 0.01  # magnetic field strength
 t = 2.2   # temperature
 
-# Evaluating matrix elements (Taken from weaver paper)
+# Evaluating nonzero matrix elements (Taken from weaver paper)
 aa = np.exp((2 - b) / t )
 bb = np.exp(-b/t)
 cc = np.exp(-(2+b) / t)
@@ -15,11 +30,11 @@ bb_inv = np.exp(b/t)
 cc_inv = np.exp((2+b) / t)
 
 # Initialize transfer matrix
-trans_mat = np.zeros((2**n,2**n))
+trans_mat = np.zeros((d,d))
 # Populate matrix
-for i in range((2**n)//2):
+for i in range(d // 2):
     # Populating the top half of the matrix
-    if i < (2**n)//4:
+    if i < d // 4:
         trans_mat[2*i][i] = aa
         trans_mat[2*i + 1][i] = bb
         trans_mat[2*i][i + 2**(n-1)] = aa_inv
