14da4209b0e0add218309317ae8e2aa36aa2a193
Alan Robledo <ar6138@nyu.edu>
2019-09-26 18:52:01 -0400

    removed useless variables

--- nump_eig.py
+++ nump_eig.py
@@ -13,38 +13,13 @@
 # For n = 5:
 # Dominant eigenvalue is: (2.6132783525854255+0j)
 #===============================================================================
-import numpy as np
+import generate
 from numpy import linalg as la
 
 n = 5   # number of spins
-d = 2**n  # matrix dimension dxd
 b = 0.01  # magnetic field strength
 t = 2.2   # temperature
 
-# Evaluating nonzero matrix elements (Taken from weaver paper)
-aa = np.exp((2 - b) / t )
-bb = np.exp(-b/t)
-cc = np.exp(-(2+b) / t)
-aa_inv = np.exp(-(2 - b) / t )
-bb_inv = np.exp(b/t)
-cc_inv = np.exp((2+b) / t)
-
-# Initialize transfer matrix
-trans_mat = np.zeros((d,d))
-# Populate matrix
-for i in range(d // 2):
-    # Populating the top half of the matrix
-    if i < d // 4:
-        trans_mat[2*i][i] = aa
-        trans_mat[2*i + 1][i] = bb
-        trans_mat[2*i][i + 2**(n-1)] = aa_inv
-        trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
-    # Populating the bottom half of the matrix
-    else:
-        trans_mat[2*i][i] = bb
-        trans_mat[2*i + 1][i] = cc
-        trans_mat[2*i][i + 2**(n-1)] = bb_inv
-        trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
-
+trans_mat = generate.tran_mat(n, b, t)
 w, v = la.eig(trans_mat)
 print("The dominant eigenvalue is:", max(w))
--- pow_iter.py
+++ pow_iter.py
@@ -22,33 +22,6 @@
 import numpy as np
 import generate
 
-def get_tran_mat(n, b, t):
-    # Evaluating matrix elements (Taken from weaver paper)
-    aa = np.exp((2 - b) / t )
-    bb = np.exp(-b/t)
-    cc = np.exp(-(2+b) / t)
-    aa_inv = np.exp(-(2 - b) / t )
-    bb_inv = np.exp(b/t)
-    cc_inv = np.exp((2+b) / t)
-
-    # Initialize transfer matrix
-    trans_mat = np.zeros((d,d))
-    # Populate matrix
-    for i in range((2**n)//2):
-        # Populating the top half of the matrix
-        if i < (2**n)//4:
-            trans_mat[2*i][i] = aa
-            trans_mat[2*i + 1][i] = bb
-            trans_mat[2*i][i + 2**(n-1)] = aa_inv
-            trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
-        # Populating the bottom half of the matrix
-        else:
-            trans_mat[2*i][i] = bb
-            trans_mat[2*i + 1][i] = cc
-            trans_mat[2*i][i + 2**(n-1)] = bb_inv
-            trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
-    return trans_mat
-
 def power_iter(max_iterations, tolerance, matrix, vec):
     # Finding l-infinity norm of initial vector x
     xp = abs(max(vec, key=abs))
