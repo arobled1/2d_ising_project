b1ffb7ac53443b05dceefd9874b5bf92fc7c1b81
Alan Robledo <ar6138@nyu.edu>
2019-09-26 18:07:21 -0400

    moved variables

--- pow_iter.py
+++ pow_iter.py
@@ -21,7 +21,39 @@
 #===============================================================================
 import numpy as np
 
+def get_tran_mat(n, b, t):
+    # matrix dimension dxd
+    d = 2**n
+    # Evaluating matrix elements (Taken from weaver paper)
+    aa = np.exp((2 - b) / t )
+    bb = np.exp(-b/t)
+    cc = np.exp(-(2+b) / t)
+    aa_inv = np.exp(-(2 - b) / t )
+    bb_inv = np.exp(b/t)
+    cc_inv = np.exp((2+b) / t)
+
+    # Initialize transfer matrix
+    trans_mat = np.zeros((d,d))
+    # Populate matrix
+    for i in range((2**n)//2):
+        # Populating the top half of the matrix
+        if i < (2**n)//4:
+            trans_mat[2*i][i] = aa
+            trans_mat[2*i + 1][i] = bb
+            trans_mat[2*i][i + 2**(n-1)] = aa_inv
+            trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
+        # Populating the bottom half of the matrix
+        else:
+            trans_mat[2*i][i] = bb
+            trans_mat[2*i + 1][i] = cc
+            trans_mat[2*i][i + 2**(n-1)] = bb_inv
+            trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
+    return trans_mat
+
 def power_iter(max_iterations, tolerance, matrix, vec):
+    # Setting initial guess vector
+    init_vec = np.zeros(d)
+    init_vec[0] = 1
     # Finding l-infinity norm of initial vector x
     xp = abs(max(vec, key=abs))
     # Rescaling the initial vector to prevent from blowing up
@@ -51,41 +83,13 @@ def power_iter(max_iterations, tolerance, matrix, vec):
     return eig_val, vec
 
 n = 5    # number of spins
-d = 2**n  # matrix dimension dxd
 b = 0.01  # magnetic field strength
 t = 2.2   # temperature
-
-# Evaluating matrix elements (Taken from weaver paper)
-aa = np.exp((2 - b) / t )
-bb = np.exp(-b/t)
-cc = np.exp(-(2+b) / t)
-aa_inv = np.exp(-(2 - b) / t )
-bb_inv = np.exp(b/t)
-cc_inv = np.exp((2+b) / t)
-
-# Initialize transfer matrix
-trans_mat = np.zeros((d,d))
-# Populate matrix
-for i in range((2**n)//2):
-    # Populating the top half of the matrix
-    if i < (2**n)//4:
-        trans_mat[2*i][i] = aa
-        trans_mat[2*i + 1][i] = bb
-        trans_mat[2*i][i + 2**(n-1)] = aa_inv
-        trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
-    # Populating the bottom half of the matrix
-    else:
-        trans_mat[2*i][i] = bb
-        trans_mat[2*i + 1][i] = cc
-        trans_mat[2*i][i + 2**(n-1)] = bb_inv
-        trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
 
 # Max number of iterations (set by user)
 max_iter = 200
 # Tolerance (set by user)
 tol = 10**-3
-# Setting initial guess vector
-init_vec = np.zeros(d)
-init_vec[0] = 1
 
+get_tran_mat(n, b, t)
 power_iter(max_iter, tol, trans_mat, init_vec)
