6f74c86b457c4c3d96870722bb334ba095f4b797
Alan Robledo <ar6138@nyu.edu>
2019-09-26 17:19:03 -0400

    added code (needs comments)

---
+++ nump_eig.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numpy import linalg as la
+
+n = 50    # number of spins
+d = 2**n  # matrix dimension dxd
+b = 0.01  # magnetic field strength
+t = 2.2   # temperature
+
+# Evaluating matrix elements (Taken from weaver paper)
+aa = np.exp((2 - b) / t )
+bb = np.exp(-b/t)
+cc = np.exp(-(2+b) / t)
+aa_inv = np.exp(-(2 - b) / t )
+bb_inv = np.exp(b/t)
+cc_inv = np.exp((2+b) / t)
+
+# Initialize transfer matrix
+trans_mat = np.zeros((2**n,2**n))
+# Populate matrix
+for i in range((2**n)//2):
+    # Populating the top half of the matrix
+    if i < (2**n)//4:
+        trans_mat[2*i][i] = aa
+        trans_mat[2*i + 1][i] = bb
+        trans_mat[2*i][i + 2**(n-1)] = aa_inv
+        trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
+    # Populating the bottom half of the matrix
+    else:
+        trans_mat[2*i][i] = bb
+        trans_mat[2*i + 1][i] = cc
+        trans_mat[2*i][i + 2**(n-1)] = bb_inv
+        trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
+
+w, v = la.eig(trans_mat)
+print("The dominant eigenvalue is:", max(w))
