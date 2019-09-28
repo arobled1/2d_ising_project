c46a93a64e20d35ac64372018849da64efe33f2c
Alan Robledo <ar6138@nyu.edu>
2019-09-28 05:34:00 -0400

    uses old matrix generation code now

--- nump_eig.py
+++ nump_eig.py
@@ -13,13 +13,42 @@
 # For n = 5:
 # Dominant eigenvalue is: (2.6132783525854255+0j)
 #===============================================================================
-import generate
 from numpy import linalg as la
 
-n = 5   # number of spins
+def tran_mat(n, b, t):
+    # matrix dimension dxd
+    d = 2**n
+    # Evaluating matrix elements (Taken from weaver paper)
+    aa = np.exp((2 - b) / t )
+    bb = np.exp(-b/t)
+    cc = np.exp(-(2+b) / t)
+    aa_inv = np.exp(-(2 - b) / t )
+    bb_inv = np.exp(b/t)
+    cc_inv = np.exp((2+b) / t)
+
+    # Initialize transfer matrix
+    trans_mat = np.zeros((d,d))
+    for i in range((2**n)//2):
+        # Populating the top half of the matrix
+        if i < (2**n)//4:
+            trans_mat[2*i][i] = aa
+            trans_mat[2*i + 1][i] = bb
+            trans_mat[2*i][i + 2**(n-1)] = aa_inv
+            trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
+        # Populating the bottom half of the matrix
+        else:
+            trans_mat[2*i][i] = bb
+            trans_mat[2*i + 1][i] = cc
+            trans_mat[2*i][i + 2**(n-1)] = bb_inv
+            trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
+        return trans_mat
+
+n = 3   # number of spins
 b = 0.01  # magnetic field strength
 t = 2.2   # temperature
 
 trans_mat = generate.tran_mat(n, b, t)
+# Delete line below so it doesn't modify on github
+print(trans_mat)
 w, v = la.eig(trans_mat)
 print("The dominant eigenvalue is:", max(w))
