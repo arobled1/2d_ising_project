b74eb9dd14eb3ca80daf94fde82b7b34772fbdd5
Alan Robledo <ar6138@nyu.edu>
2019-09-28 01:09:54 -0400

    makes transfer matrix in csr format

--- generate.py
+++ generate.py
@@ -14,17 +14,37 @@ def tran_mat(n, b, t):
     # Initialize transfer matrix
     trans_mat = np.zeros((d,d))
     # Populate matrix
-    for i in range((2**n)//2):
-        # Populating the top half of the matrix
-        if i < (2**n)//4:
-            trans_mat[2*i][i] = aa
-            trans_mat[2*i + 1][i] = bb
-            trans_mat[2*i][i + 2**(n-1)] = aa_inv
-            trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
-        # Populating the bottom half of the matrix
+
+    val = []
+    for i in range(d//2):
+        if i < d //4:
+            val.append(aa)
+            val.append(aa_inv)
+            val.append(bb)
+            val.append(bb_inv)
         else:
-            trans_mat[2*i][i] = bb
-            trans_mat[2*i + 1][i] = cc
-            trans_mat[2*i][i + 2**(n-1)] = bb_inv
-            trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
+            val.append(bb)
+            val.append(bb_inv)
+            val.append(cc)
+            val.append(cc_inv)
+    col = []
+    for i in range(d):
+        col.append(i)
+        col.append(i+4)
+        col.append(i)
+        col.append(i+4)
+    row_ptr = [x for x in range(1,2*d,2)]
+    # for i in range((2**n)//2):
+    #     # Populating the top half of the matrix
+    #     if i < (2**n)//4:
+    #         trans_mat[2*i][i] = aa
+    #         trans_mat[2*i + 1][i] = bb
+    #         trans_mat[2*i][i + 2**(n-1)] = aa_inv
+    #         trans_mat[2*i + 1][i + 2**(n-1)] = bb_inv
+    #     # Populating the bottom half of the matrix
+    #     else:
+    #         trans_mat[2*i][i] = bb
+    #         trans_mat[2*i + 1][i] = cc
+    #         trans_mat[2*i][i + 2**(n-1)] = bb_inv
+    #         trans_mat[2*i + 1][i + 2**(n-1)] = cc_inv
     return trans_mat
